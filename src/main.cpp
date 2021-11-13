64da9ccffe52d10e01d7b4ebfd6cba32a7be3351
Jeff Wang <wwong728@163.com>
2021-11-13 11:05:58 +0800

    QGraphicsView first submit

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,31 @@
 #include <QApplication>
+#include <QtWidgets>
+#include <QtOpenGL>
 
 int main(int argc, char* argv[])
 {
 	QApplication app(argc, argv);
+
+	QGraphicsScene scene;
+	QGraphicsView view;
+	view.setViewport(new QGLWidget(QGLFormat(QGL::SampleBuffers)));
+	view.setScene(&scene);
+	view.setWindowTitle(QApplication::translate("simpleanchorlayout", "Simple Anchor Layout"));
+	view.setDragMode(QGraphicsView::RubberBandDrag);
+
+	QGraphicsRectItem *item1 = new QGraphicsRectItem(0, 0, 100, 100);
+	item1->setBrush(QBrush(Qt::red));
+	item1->setFlag(QGraphicsItem::ItemIsMovable);
+	item1->setFlag(QGraphicsItem::ItemIsSelectable);
+
+	QImage image("/home/jeff/Downloads/BingWallpaper.jpg");
+	QGraphicsPixmapItem *item2 = new QGraphicsPixmapItem(QPixmap::fromImage(image));
+
+	scene.addItem(item2);
+	scene.addItem(item1);
+
+	view.resize(360, 320);
+	view.show();
 
 	return app.exec();
 }
